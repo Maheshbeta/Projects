0e00ce1f5e891799be3ccce2a32beaf7bc9986b1
Maheshbeta <134756998+Maheshbeta@users.noreply.github.com>
2023-05-29 16:12:43 +1000

    Update Bandwidth Tracker.py

--- Bandwidth Tracker.py
+++ Bandwidth Tracker.py
@@ -2,39 +2,38 @@ import psutil
 import time
 import matplotlib.pyplot as plt
 
-#Creating sets for the data to be stored
-def data_usage():
+def track_data_usage():
     data_usage = []
     timestamps = []
 
-
-    #Initial network stats
+    # Initial network stats
     initial_stats = psutil.net_io_counters()
     start_time = time.time()
 
-while True:
-    #Current network stats
-    current_stats = psutil.net_io_counters()
-    elapsed_time = time.time() - start_time
+    while True:
+        # Current network stats
+        current_stats = psutil.net_io_counters()
+        elapsed_time = time.time() - start_time
 
-    #Calculate data usage since the last check
-    upload =  current_stats.bytes_sent - initial_stats.bytes_sent
-    download = current_stats.bytes_recv - initial_stats.bytes_recv
+        # Calculate data usage since the last check
+        upload = current_stats.bytes_sent - initial_stats.bytes_sent
+        download = current_stats.bytes_recv - initial_stats.bytes_recv
 
-    #Append data usage and timestamp to the lists
-    data_usage.append((upload, download))
-    timestamps.append(elapsed_time)
+        # Append data usage and timestamp to the lists
+        data_usage.append((upload, download))
+        timestamps.append(elapsed_time)
 
-    #Update initial statistics for the next iteration
-    initial_stats = current_stats
+        # Update initial statistics for the next iteration
+        initial_stats = current_stats
 
-    #Check if the user wants to stop tracking
-    stop_tracking = input("Press q to stop tracking or any other key to continue: ")
-    if stop_tracking.lower() == 'q':
-        break
+        # Check if the user wants to stop tracking
+        stop_tracking = input("Press 'q' to stop tracking or any other key to continue: ")
+        if stop_tracking.lower() == 'q':
+            break
 
-    #Generate a graph showing the data usage over time
-    plot.graph(timestamps,data_usage)
+    # Generate a graph showing the data usage over time
+    plot_graph(timestamps, data_usage)
+
 
 def plot_graph(timestamps, data_usage):
     uploads = [upload for upload, _ in data_usage]
@@ -45,11 +44,12 @@ def plot_graph(timestamps, data_usage):
 
     plt.xlabel('Time (seconds)')
     plt.ylabel('Data Usage (bytes)')
-    plt.title('Date Usage During Online Session')
+    plt.title('Data Usage During Online Session')
     plt.legend()
     plt.grid(True)
     plt.show()
 
-    #Start tracking data usage
-    track_data_usage()
-    
+# Start tracking data usage
+track_data_usage()
+
+#Psutil may not function correctly if user does not have access to network data
